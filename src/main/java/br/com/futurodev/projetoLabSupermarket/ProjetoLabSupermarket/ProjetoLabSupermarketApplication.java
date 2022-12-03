d2714cfb9998969992642688e846e92054ea35cd
Filipe Oliveira <faos_oliveira@live.com>
2022-12-03 15:58:40 -0300

    projeto criado

---
+++ src/main/java/br/com/futurodev/projetoLabSupermarket/ProjetoLabSupermarket/ProjetoLabSupermarketApplication.java
@@ -0,0 +1,13 @@
+package br.com.futurodev.projetoLabSupermarket.ProjetoLabSupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLabSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLabSupermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/futurodev/projetoLabSupermarket/ProjetoLabSupermarket/ProjetoLabSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.futurodev.projetoLabSupermarket.ProjetoLabSupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLabSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
